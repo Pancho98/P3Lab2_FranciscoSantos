bcaf664e42cadfb70098fa7041f411d8a7bed33d
Francisco Santos <francisco@localhost.localdomain>
2018-02-02 14:15:50 -0600

    Metodos de los ejercicios

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
+#include<cstdlib>
+#include<math.h>
 #include <iostream>
 using namespace std;
 
 //Primer Problema
 void Primero();
 //Segundo Problema
-void segundo();
+void Segundo();
 //Tercer Problema
 void Tercero();
 //
@@ -16,12 +18,12 @@ void Tercero();
 int main(){
 	int opcion =1;	
 	while(opcion!=4){
-		cout<< "------MENU------"<<endl;
+		cout<< "----------MENU----------"<<endl;
 		cout<< "Ingrese la opcion a Realizar"<<endl;
 		cout<< "1. Primer Ejercio"<<endl;
 		cout<< "2. Segundo Ejercicio "<<endl;
 		cout<< "3. Tercer Ejercicio"<<endl;
-		cout<< "4. Salir del Programa";
+		cout<< "4. Salir del Programa"<<endl;
 		cin>>opcion;	
 		switch(opcion){
 			case 1:
@@ -55,6 +57,38 @@ void Primero(){
 		cout<<"3. Calcular Mayor Triangular"<<endl; 
 		cout<<"4. Regresar"<<endl;
 		cin>>opcion;
+		
+		switch(opcion){
+			case 1:
+
+				   
+	        break;
+			case 2:
+
+
+			break;
+			case 3:
+
+
+			break;
+		}
 	}
 }
 
+void Segundo(){
+	int lado_a,lado_b,lado_c;
+	cout<<"Ingrese el lado a: "<<endl;
+	cin>>lado_a;
+	cout<<"Ingrese el lado b: "<<endl;
+	cin>>lado_b;
+	cout<<"Ingrese el lado c: "<<endl;
+	cin>>lado_c;
+	
+	
+}
+
+void Tercero(){
+	int secuencia;
+	cout<<"Ingrese el numero que desee que llegue la secuencia catalan: "<<endl;
+	cin>>secuencia;
+}
